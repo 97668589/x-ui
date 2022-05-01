5ed4a365a655c13607824a32c8d06ece91784fcd
97668589 <56907698+97668589@users.noreply.github.com>
2022-05-02 00:11:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,10 +35,17 @@ if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
     arch="amd64"
 elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
     arch="arm64"
+elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
 else
-    arch="amd64"
-    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+    echo -e "不支持的CPU架构！脚本将自动退出！"
+    rm -f install.sh
+    exit 1
 fi
+# else
+    # arch="amd64"
+    # echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+# fi
 
 echo "架构: ${arch}"
 
